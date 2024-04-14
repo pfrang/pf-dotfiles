634f133836e7951c05eb5dd186b8181db1e5c4fe
Peder Frang <peder.frang@gmail.com>
2024-04-14 11:26:54 +0200

    Push pf dotfiles

---
+++ install.sh
@@ -0,0 +1,16 @@
+# Install zsh-syntax-highlighting plugin
+CURRENT_DIR=`pwd`
+ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
+mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
+if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
+  echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'..."
+  git clone https://github.com/zsh-users/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting
+fi
+cd "$CURRENT_DIR"
+
+#Add github copilot - enables command ghcs and ghce
+echo 'eval "$(gh copilot alias -- zsh)"' >> ~/.zshrc
+
+# Refresh the current terminal with the newly installed configuration
+zsh ~/.zshrc
